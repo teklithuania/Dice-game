b395016cf491996b573014fd639434e7b203e411
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-03-25 17:42:18 +0200

    Draw button

--- js/dice.js
+++ js/dice.js
@@ -1,25 +1,27 @@
 "use strict"
 
 
-let randomNumber1 = Math.floor(Math.random() * 6);
-randomNumber1 = randomNumber1 + 1;
-console.log(randomNumber1);
 
-document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelector('button').addEventListener('click', drawDice);
 
-let randomNumber2 = Math.floor(Math.random() * 6);
-randomNumber2 = randomNumber2 + 1;
-console.log(randomNumber2);
-
-document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
-
-if ( randomNumber1 > randomNumber2 ) {
-    document.querySelector('h1').innerHTML = '<img class="imgWinner" src="./images/WINNER.png">' + 'Player 1 Winner!';
-    document.querySelector('h1').style.fontSize = '100px';
-} else if ( randomNumber1 < randomNumber2 ) {
-    document.querySelector('h1').innerHTML = 'Player 2 Winner!' + '<img class="imgWinner" src="./images/WINNER.png">';
-    document.querySelector('h1').style.fontSize = '100px';
-} else {
-    document.querySelector('h1').innerHTML = 'Draw!';
-    document.querySelector('h1').style.fontSize = '100px';
+function drawDice(){
+    let randomNumber1 = Math.floor(Math.random() * 6);
+    randomNumber1 = randomNumber1 + 1;
+    console.log(randomNumber1);
+    document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+    let randomNumber2 = Math.floor(Math.random() * 6);
+    randomNumber2 = randomNumber2 + 1;
+    console.log(randomNumber2);
+    document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
+    
+    if ( randomNumber1 > randomNumber2 ) {
+        document.querySelector('h1').innerHTML = '<img class="imgWinner" src="./images/WINNER.png">' + 'Player 1 Winner!';
+        // document.querySelector('h1').style.fontSize = '100px';
+    } else if ( randomNumber1 < randomNumber2 ) {
+        document.querySelector('h1').innerHTML = 'Player 2 Winner!' + '<img class="imgWinner" src="./images/WINNER.png">';
+        // document.querySelector('h1').style.fontSize = '100px';
+    } else {
+        document.querySelector('h1').innerHTML = 'Draw!';
+        // document.querySelector('h1').style.fontSize = '100px';
+    }
 }
